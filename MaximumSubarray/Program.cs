68779a4b6976e879a7e7d87804835b5533c798b2
youyouxin <huang_xianjun@live.cn>
2017-04-23 15:06:01 +0800

    Find Maximum Subarray

--- MaximumSubarray/Program.cs
+++ MaximumSubarray/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace MaximumSubarray
 {
     class Program
@@ -8,7 +7,7 @@ namespace MaximumSubarray
         {
             int[] A = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
             (int low, int high, int maxSum) = MaximumSubarray.findMaximumSubarray(A, 0, A.Length - 1);
-            Console.WriteLine("low={0},high={1},maxSum={2}", low, high, maxSum);
+            System.Console.WriteLine("low={0},high={1},maxSum={2}", low, high, maxSum);
         }
     }
 }
