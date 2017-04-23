55ddc7714b770058ab86c0a9d9868ae6e5e09746
unknown <>
2017-04-23 13:00:55 +0800

    添加项目文件。

---
+++ MaximumSubarray/MaximumSubarray.cs
@@ -0,0 +1,55 @@
+﻿
+namespace MaximumSubarray
+{
+    public class MaximumSubarray
+    {
+        private static (int ,int ,int) findMaxCrossingSubarray(int[] A, int low, int mid, int high)
+        {
+            int leftSum = int.MinValue;
+            int sum = 0;
+            int maxLeft = 0;
+            for(int i = mid; i >= low; i--)
+            {
+                sum += A[i];
+                if (sum > leftSum)
+                {
+                    leftSum = sum;
+                    maxLeft = i;
+                }
+            }
+            int rightSum = int.MinValue;
+            sum = 0;
+            int maxRight = 0;
+            for(int j = mid + 1; j <= high; j++)
+            {
+                sum += A[j];
+                if (sum > rightSum)
+                {
+                    rightSum = sum;
+                    maxRight = j;
+                }
+            }
+            return (maxLeft, maxRight, leftSum + rightSum);
+        }
+        public static (int,int,int) findMaximumSubarray(int[] A,int low,int high)
+        {
+            if(high == low)
+            {
+                return (low, high, A[low]);
+            }
+            else
+            {
+                int mid = (low + high) / 2;
+                (int leftLow, int leftHigh, int leftSum) = findMaximumSubarray(A, low, mid);
+                (int rightLow, int rightHigh, int rightSum) = findMaximumSubarray(A, mid + 1, high);
+                (int crossLow, int crossHigh, int crossSum) = findMaxCrossingSubarray(A, low, mid, high);
+                if (leftSum >= rightSum && leftSum >= crossSum)
+                    return (leftLow, leftHigh, leftSum);
+                else if (rightSum >= leftSum && rightSum >= crossSum)
+                    return (rightLow, rightHigh, rightSum);
+                else
+                    return (crossLow, crossHigh, crossSum);
+            }
+        }
+    }
+}
---
+++ MaximumSubarray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaximumSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+            (int low, int high, int maxSum) = MaximumSubarray.findMaximumSubarray(A, 0, A.Length - 1);
+            Console.WriteLine("low={0},high={1},maxSum={2}", low, high, maxSum);
+        }
+    }
+}
